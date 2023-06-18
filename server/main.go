d656adab598b546f7616d734f205aa2a7abaf48b
ghjattu <icracked369@gmail.com>
2023-06-18 12:32:20 +0800

    🐞 fix(server/main.go): using cors middleware

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"server/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
@@ -19,6 +20,7 @@ func init() {
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New())
 
 	routes.RegisterRouter(app)
 
