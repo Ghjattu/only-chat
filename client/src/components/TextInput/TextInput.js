118e517406e9fdfdcfeb3366fc20ddc168a3c68d
ghjattu <icracked369@gmail.com>
2023-06-18 12:15:07 +0800

    ✨ feat(client): register and login form

---
+++ client/src/components/TextInput/TextInput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useField } from 'formik';
+import './TextInput.css';
+
+const TextInput = ({ label, ...props }) => {
+	const [field, meta] = useField(props);
+	return (
+		<div>
+			<label htmlFor={props.id}>{label}</label>
+			<input className='' {...field} {...props} />
+			{meta.touched && meta.error && <p>{meta.error}</p>}
+		</div>
+	);
+};
+
+export default TextInput;
