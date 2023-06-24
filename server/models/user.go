725ed3e8150f4c284fe79b1cb1a0afc048449995
ghjattu <icracked369@gmail.com>
2023-06-24 18:59:37 +0800

    ✨ feat(server/models/user): omit password when return user

--- server/models/user.go
+++ server/models/user.go
@@ -15,10 +15,15 @@ var chatID int = 100000
 
 type User struct {
 	gorm.Model
-	ChatID   string `json:"chat_id" gorm:"unique;not null"`
+	ChatID   string `json:"chatid" gorm:"unique;not null"`
 	Username string `json:"username" gorm:"size:30;not null"`
 	Password string `json:"password" gorm:"size:255;not null"`
-	Friends  []User `json:"friends" gorm:"many2many:user_friends"`
+}
+
+type APIUser struct {
+	ID       uint   `json:"id"`
+	ChatID   string `json:"chatid"`
+	Username string `json:"username"`
 }
 
 // BeforeCreate is a gorm hook that hashes user's password
@@ -37,28 +42,34 @@ func (u *User) BeforeCreate(tx *gorm.DB) error {
 }
 
 // CreateNewUser creates a new user.
-func CreateNewUser(u *User) (*User, error) {
+func CreateNewUser(u *User) (*APIUser, error) {
 	err := db.Create(u).Error
 	if err != nil {
-		return &User{}, err
+		return &APIUser{}, err
 	}
 
 	// If a new user is created successfully, then increase the chatID.
 	chatID++
 
-	return u, nil
+	returnedUser := &APIUser{
+		ID:       u.ID,
+		ChatID:   u.ChatID,
+		Username: u.Username,
+	}
+
+	return returnedUser, nil
 }
 
 // GetUsersByUsername retrieves all users whose username
 // matches the specified pattern "%s%".
 // Here, the "%" is a wild card which represents zero, one or multiple characters.
-func GetUsersByPattern(s string) ([]User, error) {
+func GetUsersByPattern(s string) ([]APIUser, error) {
 	pattern := "%" + s + "%"
-	users := make([]User, 0)
+	users := make([]APIUser, 0)
 
 	err := db.Where("username LIKE ?", pattern).Find(&users).Error
 	if err != nil {
-		return []User{}, err
+		return []APIUser{}, err
 	}
 
 	return users, nil
@@ -77,18 +88,22 @@ func GetUserByID(id uint) (*User, error) {
 }
 
 // GetUserByChatID finds a user by ChatID.
-func GetUserByChatID(chatID string) (*User, error) {
+func GetUserByChatID(chatID string, omitPassword bool) (*APIUser, string, error) {
 	user := &User{}
 
 	err := db.Where("chat_id = ?", chatID).First(user).Error
 	if err != nil {
-		return &User{}, err
+		return &APIUser{}, "", err
 	}
 
-	return user, nil
-}
+	returnedUser := &APIUser{
+		ID:       user.ID,
+		ChatID:   user.ChatID,
+		Username: user.Username,
+	}
 
-// AddContact established a two-way friend relationship between two users.
-func AddContact(id1, id2 uint) error {
-	return nil
+	if omitPassword {
+		return returnedUser, "", nil
+	}
+	return returnedUser, user.Password, nil
 }
