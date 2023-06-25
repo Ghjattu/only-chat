d33a72d08d4f6dc4af74e21c8beb335fcfaba5d1
ghjattu <icracked369@gmail.com>
2023-06-25 11:00:33 +0800

    🦄 refactor(server/utils/validatelogin):

--- server/utils/validatelogin.go
+++ server/utils/validatelogin.go
@@ -6,17 +6,21 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// ValidateLogin validates the user's identity
-// and returns the user ID if it passes.
+// ValidateLogin validates the user's identity and returns the id if it passes.
+//
+//	@param chatID
+//	@param password
+//	@return uint "id"
+//	@return error
 func ValidateLogin(chatID, password string) (uint, error) {
 	// Use the chatID to find the specified user because it is unique.
-	user, err := models.GetUserByChatID(chatID)
+	user, hashedPassword, err := models.GetUserByChatID(chatID, false)
 	if err != nil {
 		return 0, err
 	}
 
 	// Check whether the password is correct.
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	if err != nil {
 		return 0, err
 	}
