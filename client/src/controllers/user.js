d0ae80bdbf372dd44a2809d67674f8805b889977
ghjattu <icracked369@gmail.com>
2023-06-25 16:01:20 +0800

    ✨ feat(client/src/controllers/user): handle error

--- client/src/controllers/user.js
+++ client/src/controllers/user.js
@@ -3,13 +3,21 @@ import axios from 'axios';
 const baseUrl = 'http://localhost:8080/api/v1';
 
 const register = async (user) => {
-	const res = await axios.post(`${baseUrl}/register`, user);
-	console.log(res);
+	try {
+		const res = await axios.post(`${baseUrl}/register`, user);
+		return res.data;
+	} catch (error) {
+		return error.response.data;
+	}
 };
 
 const login = async (user) => {
-	const res = await axios.post(`${baseUrl}/login`, user);
-	console.log(res);
+	try {
+		const res = await axios.post(`${baseUrl}/login`, user);
+		return res.data;
+	} catch (error) {
+		return error.response.data;
+	}
 };
 
 const userControllers = { register, login };
