ab1fa921cbecf70b1ed6b9823b661a147cfaaaf2
ghjattu <icracked369@gmail.com>
2023-06-25 11:10:00 +0800

    ✨ feat(server/models/message): message struct

---
+++ server/models/message.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Message struct {
+	gorm.Model
+	From      uint      `json:"from"`
+	To        uint      `json:"to"`
+	Timestamp time.Time `json:"timestamp"`
+	Content   string    `json:"content"`
+}
+
+// CreateNewMessage creates a new Message.
+func CreateNewMessage(m *Message) error {
+	err := db.Create(m).Error
+	return err
+}
