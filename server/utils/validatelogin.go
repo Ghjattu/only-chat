faf80f1006783c4b180b6969125f1be95e8b15b3
ghjattu <icracked369@gmail.com>
2023-06-26 16:35:50 +0800

    ✨ feat(server/utils): modify return value

--- server/utils/validatelogin.go
+++ server/utils/validatelogin.go
@@ -6,24 +6,24 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// ValidateLogin validates the user's identity and returns the id if it passes.
+// ValidateLogin validates the user's identity and returns the user if it passes.
 //
 //	@param chatID
 //	@param password
-//	@return uint "id"
+//	@return *models.APIUser
 //	@return error
-func ValidateLogin(chatID, password string) (uint, error) {
+func ValidateLogin(chatID, password string) (*models.APIUser, error) {
 	// Use the chatID to find the specified user because it is unique.
 	user, hashedPassword, err := models.GetUserByChatID(chatID, false)
 	if err != nil {
-		return 0, err
+		return &models.APIUser{}, err
 	}
 
 	// Check whether the password is correct.
 	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	if err != nil {
-		return 0, err
+		return &models.APIUser{}, err
 	}
 
-	return user.ID, nil
+	return user, nil
 }
