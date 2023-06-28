187e11709e1bce12f6f343856d91191bb7ed5e1d
ghjattu <icracked369@gmail.com>
2023-06-28 11:10:31 +0800

    ✨ feat(server/ws): websocket client

---
+++ server/ws/client.go
@@ -0,0 +1,42 @@
+package ws
+
+import (
+	"log"
+	"server/models"
+
+	"github.com/gofiber/contrib/websocket"
+)
+
+// Client indicates a user in a websocket connection.
+type Client struct {
+	ID       uint
+	ChatID   string
+	Username string
+	Conn     *websocket.Conn
+	Hub      *Hub
+}
+
+// Read listens in for new messages coming through on this client’s websocket connection.
+//
+//	@receiver c
+func (c *Client) Read() {
+	defer func() {
+		c.Hub.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		msg := &models.Message{}
+
+		err := c.Conn.ReadJSON(msg)
+		if err != nil {
+			log.Println("websocket read error:", err.Error())
+			break
+		}
+
+		err = models.CreateNewMessage(msg)
+		if err == nil {
+			c.Hub.MsgQueue <- msg
+		}
+	}
+}
