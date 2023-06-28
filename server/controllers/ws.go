3f0de3a7275e9dcc8874d1458b07282a2558c596
ghjattu <icracked369@gmail.com>
2023-06-28 11:12:32 +0800

    ✨ feat(server/controllers): websocket handler

---
+++ server/controllers/ws.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"os"
+	"server/ws"
+	"strconv"
+
+	"github.com/gofiber/contrib/websocket"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+var hub *ws.Hub
+
+func init() {
+	size, _ := strconv.Atoi(os.Getenv("MSG_QUEUE_SIZE"))
+	if size == 0 {
+		size = 100
+	}
+
+	hub = ws.NewHub(size)
+
+	go hub.Start()
+}
+
+// WebSocketHandler handles the websocket connection.
+var WebSocketHandler = websocket.New(func(c *websocket.Conn) {
+	defer func() {
+		c.Close()
+	}()
+
+	// get the route parameters.
+	id, err := strconv.Atoi(c.Params("id"))
+	chatid := c.Params("chatid")
+	username := c.Params("username")
+
+	if err != nil {
+		return
+	}
+
+	client := &ws.Client{
+		ID:       uint(id),
+		ChatID:   chatid,
+		Username: username,
+		Conn:     c,
+		Hub:      hub,
+	}
+	hub.Register <- client
+	client.Read()
+})
