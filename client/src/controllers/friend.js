4d826e0a471140b958c9f959eebbc40adb492146
ghjattu <icracked369@gmail.com>
2023-07-07 15:19:36 +0800

    🐞 fix: url error

--- client/src/controllers/friend.js
+++ client/src/controllers/friend.js
@@ -4,7 +4,7 @@ const baseUrl = 'http://localhost:8080/api/v1';
 
 const getAllFriend = async (id) => { 
 	try {
-		const res = await axios.get(`${baseUrl}/friend/${id}`);
+		const res = await axios.get(`${baseUrl}/friends/${id}`);
 		console.log(res);
 		return res.data;
 	} catch (error) {
