46d97eb739130a2f2c90b1cfed1f2881f0033eb7
ghjattu <icracked369@gmail.com>
2023-07-07 12:21:13 +0800

    ✨ feat(server/controllers): get id from route parameters

--- server/controllers/friendrel.go
+++ server/controllers/friendrel.go
@@ -7,13 +7,30 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// getID gets an unsigned integer from the route parameters.
+// If the parameter is not found or cannot be converted to an integer,
+// it will return 0 and an error.
+//
+//	@param c *fiber.Ctx
+//	@param key string
+//	@return uint
+//	@return error
+func getID(c *fiber.Ctx, key string) (uint, error) {
+	id, _ := c.ParamsInt(key, 0)
+	if id <= 0 {
+		return 0, fiber.NewError(fiber.StatusBadRequest, "bad request")
+	}
+
+	return uint(id), nil
+}
+
 func GetAllFriends(c *fiber.Ctx) error {
-	id, err := c.ParamsInt("id")
+	id, err := getID(c, "id")
 	if err != nil {
 		return utils.ErrorHandler(c, err)
 	}
 
-	friends, err := models.GetAllFriendsByID(uint(id))
+	friends, err := models.GetAllFriendsByID(id)
 	if err != nil {
 		return utils.ErrorHandler(c, err)
 	}
@@ -27,17 +44,17 @@ func GetAllFriends(c *fiber.Ctx) error {
 }
 
 func AddFriend(c *fiber.Ctx) error {
-	id1, err := c.ParamsInt("id1")
+	id1, err := getID(c, "id1")
 	if err != nil {
 		return utils.ErrorHandler(c, err)
 	}
 
-	id2, err := c.ParamsInt("id2")
+	id2, err := getID(c, "id2")
 	if err != nil {
 		return utils.ErrorHandler(c, err)
 	}
 
-	err = models.AddFriend(uint(id1), uint(id2))
+	err = models.AddFriend(id1, id2)
 	if err != nil {
 		return utils.ErrorHandler(c, err)
 	}
