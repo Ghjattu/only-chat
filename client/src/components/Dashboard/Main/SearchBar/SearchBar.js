96dc82ff8bb51b1a9f1f04710b055ddaa3559d9d
ghjattu <icracked369@gmail.com>
2023-07-07 20:16:44 +0800

    ✨ feat: search bar component

--- client/src/components/Dashboard/Main/SearchBar/SearchBar.js
+++ client/src/components/Dashboard/Main/SearchBar/SearchBar.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// icons theme
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#348ee9',
+		},
+		secondary: {
+			main: '#868686',
+		},
+	},
+});
 
 const SearchBar = () => {
+	const [iconColor, setIconColor] = useState('secondary');
+	const [searchText, setSearchText] = useState('');
+
+	const handleFocus = () => {
+		setIconColor('primary');
+	};
+
+	const handleBlur = () => {
+		setIconColor('secondary');
+	};
+
+	const handleChange = (e) => {
+		setSearchText(e.target.value);
+	};
+
 	return (
-		<div>
-			<input type='text' placeholder='search'></input>
+		<div className='search-bar-wrapper'>
+			<ThemeProvider theme={theme}>
+				<SearchOutlinedIcon color={iconColor} />
+			</ThemeProvider>
+			<input className='search-bar' type='text' placeholder='Search' value={searchText}
+				onFocus={handleFocus} onBlur={handleBlur} onChange={handleChange}>
+			</input>
 		</div>
 	);
 };
